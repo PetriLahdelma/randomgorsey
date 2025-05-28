96e0e63381174571ccd7860d40a631f89cce3a3a
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-05-28 23:04:05 +0300

    Fixed hash issues

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ const Header: React.FC = () => {
       <nav>
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><a href="#/about">About</a></li>
+          <li><a href="#/contact">Contact</a></li>
         </ul>
       </nav>
     </header>
