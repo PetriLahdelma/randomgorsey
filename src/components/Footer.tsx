6aabb506a8b9610773c55627932b6e81724f9363
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-05-29 17:29:03 +0300

    Font and color updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import { HomeIcon } from '@heroicons/react/solid';
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <p>&copy; 2025 Random Gorsey. All Rights Reserved.</p>
+      <HomeIcon className={styles.homeIcon} />
     </footer>
   );
 };
