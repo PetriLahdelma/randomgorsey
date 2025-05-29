4ac5b906375af4890d262fd256210986fdacd679
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-05-29 14:33:41 +0300

    Adds newest changes to 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './NotFound.module.css'; // Import the CSS module for styling
 
 const NotFound: React.FC = () => {
   return (
-    <div>
+    <div className={styles['notfound-container']}>
       <h2>404 - Page Not Found</h2>
-      <p>Sorry, the page you're looking for does not exist.</p>
+      <p className={styles['notfound-description']}>Sorry, the page you're looking for does not exist.</p>
     </div>
   );
 };
