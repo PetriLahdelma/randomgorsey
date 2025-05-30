36b4e62a2acc21eb440f76b803d11d28fd0210b7
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-05-30 23:38:28 +0300

    Many updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,15 +6,9 @@ const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles['social-links']}>
-      <a href="https://open.spotify.com/artist/54Vv9rlCqX2nW2V0tXw33q?si=TCP19UlhTpyHb7w0UOukmg" target="_blank" rel="noopener noreferrer">
         <SocialIcon label="Spotify" url="https://open.spotify.com/artist/54Vv9rlCqX2nW2V0tXw33q?si=TCP19UlhTpyHb7w0UOukmg" bgColor="#000" fgColor="#fff" style={{ width: '2rem', height: '2rem' }} />
-      </a>
-      <a href="https://soundcloud.com/randomgorsey" target="_blank" rel="noopener noreferrer">
         <SocialIcon label="Soundcloud" url="https://soundcloud.com/randomgorsey" bgColor="#000" fgColor="#fff" style={{ width: '2rem', height: '2rem' }} />
-      </a>
-      <a href="https://www.instagram.com/random_gorsey?igsh=am42eWcwNGdnY3hm&utm_source=qr" target="_blank" rel="noopener noreferrer">
         <SocialIcon label="Instagram" url="https://www.instagram.com/random_gorsey?igsh=am42eWcwNGdnY3hm&utm_source=qr" bgColor="#000" fgColor="#fff" style={{ width: '2rem', height: '2rem' }} />
-      </a>
       </div>
       <p>&copy; 2025 Random Gorsey. All Rights Reserved.</p>
     </footer>
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './NotFound.module.css';
+ 
 
 const NotFound: React.FC = () => {
   return (
     <div className={styles['notfound-container']}>
+      {/* <video className={styles['background-video']} autoPlay muted loop>
+        <source src="/images/butterflies.webm" type="video/webm" />
+        Your browser does not support the video tag.
+      </video> */}
       <h2>404 - Page Not Found</h2>
       <p className={styles['notfound-description']}>Sorry, the page you're looking for does not exist.</p>
     </div>
