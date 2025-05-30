80bd605329cb135adfcf997908959f1164382e68
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-05-30 14:56:24 +0300

    Adds more gallery images

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,6 +12,13 @@ const About: React.FC = () => {
     <p className={styles['about-description']}>
       Helsinki-based producer/musician Random Gorsey aka DJ Pizza Hut makes warm, nostalgic lo-fi house and electronic music with soulful chords and minimalist grooves. His music pays homage to slow-paced, high-energy electronic music. <br /><br />Having spent the last few years producing for his own amusement and after his debut EP steadily brought the much-needed confidence, he decided to take a step forward and start producing on a more regular basis.  Random G doesn't feel he belongs to any particular scene. As is the nature of his music, he just acts on instinct and goes wherever it takes him and tries not to overthink it. <br /><br />Originally from Turku, Finland, he studied graphic design in the UK, before returning to Finland for his master's. While he is currently on hiatus focusing on his design career, he has always had a deep passion for making music. He loves exploring new tools, sounds, and ideas, constantly finding fresh processes in an artistic way. Mostly he is inspired by aestheticism, raw energy, and talent, always looking to solve problems and push creative boundaries.
     </p>
+      <h2>Side projects</h2>
+      <p className={styles['about-description']}>
+        Random Gorsey is also involved in several side projects, including:</p>
+        <ul>
+          <li><a href="https://soundcloud.com/petri-lahdelma" target="_blank" rel="noopener noreferrer">Petri Lahdelma</a> - A more coarse techno project.</li>
+          <li><a href="https://soundcloud.com/dj-pizza-hut" target="_blank" rel="noopener noreferrer">Dj Pizza Hut</a> - Honestly, I don't even know, man.</li>
+          </ul>
     </div>
   );
 };
--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -6,6 +6,42 @@ const Gallery: React.FC = () => {
     <div className={styles['gallery-container']}>
       <h2>Gallery</h2>
       <img
+        src="/images/ride.jpg"
+        alt="Ride in BLN"
+        style={{
+          width: '100%',
+          borderRadius: '8px',
+          marginTop: '1rem',
+        }}
+      />
+      <figcaption style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#fff' }}>
+        Ride in Birgit, Berlin
+      </figcaption>
+      <img
+        src="/images/disco.jpg"
+        alt="Disco balls in BLN"
+        style={{
+          width: '100%',
+          borderRadius: '8px',
+          marginTop: '1rem',
+        }}
+      />
+      <figcaption style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#fff' }}>
+        Disco balls in Birgit, Berlin
+      </figcaption>
+      <img
+        src="/images/xberg.jpg"
+        alt="Xberg Acid, Berlin"
+        style={{
+          width: '100%',
+          borderRadius: '8px',
+          marginTop: '1rem',
+        }}
+      />
+      <figcaption style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#fff' }}>
+        Xberg Acid, Berlin
+      </figcaption>
+      <img
         src="/images/ompputalo.jpg"
         alt="Ompputalo"
         style={{
@@ -16,6 +52,54 @@ const Gallery: React.FC = () => {
       />
       <figcaption style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#fff' }}>
         Ompputalo in Lapinlahti, Helsinki
+      </figcaption>
+      <img
+        src="/images/kuvaxtila.jpg"
+        alt="Kuva x Tila"
+        style={{
+          width: '100%',
+          borderRadius: '8px',
+          marginTop: '1rem',
+        }}
+      />
+      <figcaption style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#fff' }}>
+        Projections in Kuva x Tila, Helsinki
+      </figcaption>
+      <img
+        src="/images/laser.jpg"
+        alt="Lasers"
+        style={{
+          width: '100%',
+          borderRadius: '8px',
+          marginTop: '1rem',
+        }}
+      />
+      <figcaption style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#fff' }}>
+        Warning, Lasers, Helsinki
+      </figcaption>
+      <img
+        src="/images/smokebreak.jpg"
+        alt="Smoke break"
+        style={{
+          width: '100%',
+          borderRadius: '8px',
+          marginTop: '1rem',
+        }}
+      />
+      <figcaption style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#fff' }}>
+        Smoke break, Helsinki
+      </figcaption>
+      <img
+        src="/images/discosea.jpg"
+        alt="Disco at sea"
+        style={{
+          width: '100%',
+          borderRadius: '8px',
+          marginTop: '1rem',
+        }}
+      />
+      <figcaption style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#fff' }}>
+        Disco at sea, Helsinki
       </figcaption>
       <img
         src="/images/kaiku.jpg"
@@ -30,6 +114,18 @@ const Gallery: React.FC = () => {
         Kaiku, Helsinki
       </figcaption>
       <img
+        src="/images/freedom.jpg"
+        alt="Freedom"
+        style={{
+          width: '100%',
+          borderRadius: '8px',
+          marginTop: '1rem',
+        }}
+      />
+      <figcaption style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#fff' }}>
+        Occupied freedom, Helsinki
+      </figcaption>
+      <img
         src="/images/promo.jpg"
         alt="Kaiku"
         style={{
@@ -39,7 +135,7 @@ const Gallery: React.FC = () => {
         }}
       />
       <figcaption style={{ textAlign: 'center', marginTop: '0.5rem', fontStyle: 'italic', color: '#fff' }}>
-        Promo shot by a friend, Helsinki
+        Shot by a friend, Helsinki
       </figcaption>
     </div>
   );
