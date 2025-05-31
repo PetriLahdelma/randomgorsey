62e5ad3f3b4649db96139077ad4a688e0e3282e4
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-05-31 17:30:03 +0300

    Updates Gallery further

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { XMarkIcon, ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/solid';
 import styles from './Gallery.module.css';
 
@@ -10,21 +10,24 @@ const Gallery: React.FC<GalleryProps> = ({ onOverlayStateChange }) => {
   const [overlayImage, setOverlayImage] = useState<string | null>(null);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  const images = [
-    { src: '/images/clipart.jpg', caption: 'Clipart, Helsinki' },
-    { src: '/images/ride.jpg', caption: 'Ride in Birgit, Berlin' },
-    { src: '/images/disco.jpg', caption: 'Disco balls in Birgit, Berlin' },
-    { src: '/images/xberg.jpg', caption: 'Xberg Acid, Berlin' },
-    { src: '/images/gordon.jpg', caption: 'Random Illustration, Helsinki' },
-    { src: '/images/ompputalo.jpg', caption: 'Ompputalo in Lapinlahti, Helsinki' },
-    { src: '/images/kuvaxtila.jpg', caption: 'Projections in Kuva x Tila, Helsinki' },
-    { src: '/images/laser.jpg', caption: 'Warning, Lasers, Helsinki' },
-    { src: '/images/smokebreak.jpg', caption: 'Smoke break, Helsinki' },
-    { src: '/images/discosea.jpg', caption: 'Disco at sea, Helsinki' },
-    { src: '/images/kaiku.jpg', caption: 'Kaiku, Helsinki' },
-    { src: '/images/freedom.jpg', caption: 'Occupied freedom, Helsinki' },
-    { src: '/images/promo.jpg', caption: 'Shot by a friend, Helsinki' },
-  ];
+  const images = useMemo(
+    () => [
+      { src: '/images/clipart.jpg', caption: 'Clipart, Helsinki' },
+      { src: '/images/ride.jpg', caption: 'Ride in Birgit, Berlin' },
+      { src: '/images/disco.jpg', caption: 'Disco balls in Birgit, Berlin' },
+      { src: '/images/xberg.jpg', caption: 'Xberg Acid, Berlin' },
+      { src: '/images/gordon.jpg', caption: 'Random Illustration, Helsinki' },
+      { src: '/images/ompputalo.jpg', caption: 'Ompputalo in Lapinlahti, Helsinki' },
+      { src: '/images/kuvaxtila.jpg', caption: 'Projections in Kuva x Tila, Helsinki' },
+      { src: '/images/laser.jpg', caption: 'Warning, Lasers, Helsinki' },
+      { src: '/images/smokebreak.jpg', caption: 'Smoke break, Helsinki' },
+      { src: '/images/discosea.jpg', caption: 'Disco at sea, Helsinki' },
+      { src: '/images/kaiku.jpg', caption: 'Kaiku, Helsinki' },
+      { src: '/images/freedom.jpg', caption: 'Occupied freedom, Helsinki' },
+      { src: '/images/promo.jpg', caption: 'Shot by a friend, Helsinki' },
+    ],
+    []
+  );
 
   const openOverlay = (index: number) => {
     setOverlayImage(images[index].src);
