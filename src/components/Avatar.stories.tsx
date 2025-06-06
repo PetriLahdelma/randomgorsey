2594891be2a15ee25fdbb11b2435bfb63a284861
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-06-06 22:00:18 +0300

    Deps and avatar

---
+++ src/components/Avatar.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react-webpack5';
+import Avatar, { AvatarProps } from './Avatar';
+import PeteImg from '../../public/images/pete.jpg';
+
+export default {
+  title: 'Components/Avatar',
+  component: Avatar,
+} as Meta;
+
+const Template: StoryFn<AvatarProps> = (args: AvatarProps) => <Avatar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  avatarColor: '#000',
+  initials: 'RG',
+};
+
+export const WithImage = Template.bind({});
+WithImage.args = {
+    avatarImage: PeteImg,
+};
