de006a81a64dd0cec9c339e5def4d38cfb77db31
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-06-17 00:31:07 +0300

    Update FirstPost.tsx

--- src/posts/FirstPost.tsx
+++ src/posts/FirstPost.tsx
@@ -5,7 +5,7 @@ const FirstPost: Post = {
   title: 'First Post',
   timestamp: '01.05.2025',
   contentType: 'text',
-  body: 'Welcome to the brand new Random Gorsey blog! This space will be filled with updates about music, art and more. Stay tuned for upcoming releases and random thoughts. This first entry is here just to give you an idea how a longer post looks like when collapsed in the feed. You can expand it to read the whole thing.',
+  body: 'Welcome to the brand new Random Gorsey blog! This space will be filled with updates about music, art and more. Stay tuned for upcoming releases and random thoughts. This first entry is here just to let y\'all know what to expect in the future. Thanks for taking the time to read this, and I hope you enjoy the content that will be posted here.',
   avatarColor: '#ff0',
   author: 'Random Gorsey',
 };
