f7c02ac36152508af3e5b326fc0f493ad4642612
Petri Lahdelma <petri.lahdelma@gmail.com>
2025-06-23 22:25:47 +0300

    Update Listen.tsx

--- src/pages/Listen.tsx
+++ src/pages/Listen.tsx
@@ -41,27 +41,31 @@ const Listen: React.FC = () => {
       {loading && <Spinner />}
       <h1>Listen to Music</h1>
       <p className={styles['listen-description']}>Enjoy curated playlists and latest tracks.</p>
-      <iframe
-        title="RG player"
-        style={{ borderRadius: '12px' }}
-        src="https://open.spotify.com/embed/artist/54Vv9rlCqX2nW2V0tXw33q?utm_source=generator"
-        width="100%"
-        height="352"
-        frameBorder="0"
-        allowFullScreen={true}
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-        onLoad={handleContentLoad}
-      ></iframe>
-      <iframe style={{ borderRadius: '12px', marginTop: '1.6rem' }}
-        title="SoundCloud Player"
-        width="100%"
-        height="300"
-        scrolling="no"
-        frameBorder="no"
-        allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/2029288998&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-      ></iframe>
+      {typeof window !== 'undefined' && (
+        <>
+          <iframe
+            title="RG player"
+            style={{ borderRadius: '12px' }}
+            src="https://open.spotify.com/embed/artist/54Vv9rlCqX2nW2V0tXw33q?utm_source=generator"
+            width="100%"
+            height="352"
+            frameBorder="0"
+            allowFullScreen={true}
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+            onLoad={handleContentLoad}
+          ></iframe>
+          <iframe style={{ borderRadius: '12px', marginTop: '1.6rem' }}
+            title="SoundCloud Player"
+            width="100%"
+            height="300"
+            scrolling="no"
+            frameBorder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/2029288998&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          ></iframe>
+        </>
+      )}
       <div style={{ fontSize: '10px', color: '#cccccc', lineBreak: 'anywhere', wordBreak: 'normal', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight: 100 }}>
         <a href="https://soundcloud.com/randomgorsey" title="Random Gorsey" target="_blank" rel="noreferrer" style={{ color: '#cccccc', textDecoration: 'none' }}>Random Gorsey</a> · <a href="https://soundcloud.com/randomgorsey/sets/tuunz" title="Tuunz" target="_blank" rel="noreferrer" style={{ color: '#cccccc', textDecoration: 'none' }}>Tuunz</a>
       </div>
