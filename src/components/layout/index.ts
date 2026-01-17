7def58dbfb4005a111ac3cea234709b874bf2c7f
Petri Lahdelma <petri.lahdelma@gmail.com>
2026-01-17 14:01:40 +0200

    feat(06-01): add barrel export for layout primitives
    - Exports Stack and stackVariants
    - Exports Cluster and clusterVariants
    - Exports StackProps and ClusterProps types
    - Layout primitives importable from @/components/layout

---
+++ src/components/layout/index.ts
@@ -0,0 +1,6 @@
+// Layout primitives - composable layout components
+export { Stack, stackVariants } from "./Stack";
+export type { StackProps } from "./Stack";
+
+export { Cluster, clusterVariants } from "./Cluster";
+export type { ClusterProps } from "./Cluster";
